308a7bdb2bccee02390b89487f8fcff75fd5042b
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-12-02 23:37:08 +0000

    Import models into top level namespace
    Signed-off-by: GitHub <noreply@github.com>

--- src/onsite_edp_sdk/__init__.py
+++ src/onsite_edp_sdk/__init__.py
@@ -1,1 +1,23 @@
 """ShopWorks OnSite EDP generation."""
+
+from .models import (
+    ContactBlockModel,
+    CustomerBlockModel,
+    DesignBlockModel,
+    DesignLocationBlockModel,
+    EDPDocumentModel,
+    OrderBlockModel,
+    PaymentBlockModel,
+    ProductBlockModel,
+)
+
+__all__ = [
+    "EDPDocumentModel",
+    "OrderBlockModel",
+    "CustomerBlockModel",
+    "ContactBlockModel",
+    "DesignBlockModel",
+    "ProductBlockModel",
+    "PaymentBlockModel",
+    "DesignLocationBlockModel",
+]
