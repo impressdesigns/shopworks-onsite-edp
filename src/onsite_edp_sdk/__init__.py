5258ef7f64884446b0875e694507ea1a401a16a9
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-12-02 18:04:08 -0600

    Operate directly on the Pydantic model (#7)
    * Import models into top level namespace
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Remove redundant type information from class names
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Move serializer to class
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Run lints
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Exclude unset fields
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Make docstrings very slightly better
    
    Signed-off-by: GitHub <noreply@github.com>
    
    ---------
    
    Signed-off-by: GitHub <noreply@github.com>

--- src/onsite_edp_sdk/__init__.py
+++ src/onsite_edp_sdk/__init__.py
@@ -1,1 +1,25 @@
 """ShopWorks OnSite EDP generation."""
+
+from .models import (
+    Contact,
+    Customer,
+    Design,
+    DesignLocation,
+    EDPDocument,
+    Order,
+    Payment,
+    Product,
+)
+from .serializer import Serializer
+
+__all__ = [
+    "Contact",
+    "Customer",
+    "Design",
+    "DesignLocation",
+    "EDPDocument",
+    "Order",
+    "Payment",
+    "Product",
+    "Serializer",
+]
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,20 +1,19 @@
 """Test models."""
 
-from onsite_edp_sdk.lib import build_document
-from onsite_edp_sdk.models import CustomerBlockModel, EDPDocumentModel, OrderBlockModel
+from onsite_edp_sdk import Customer, EDPDocument, Order, Serializer
 
 
 def test_can_create_order() -> None:
-    """Can't access protected endpoints without all required scopes."""
-    edp_document = EDPDocumentModel(
-        order=OrderBlockModel(
+    """Sanity check."""
+    edp_document = EDPDocument(
+        order=Order(
             external_order_id="TEST",
             date_external="08/05/2022",
             order_type_id=1,
             date_order_placed="08/05/2022",
         ),
-        customer=CustomerBlockModel(
+        customer=Customer(
             customer_id=1,
         ),
     )
-    build_document(edp_document.model_dump(by_alias=True))
+    Serializer().build_document(edp_document)
